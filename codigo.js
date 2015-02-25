0b80143fa08910293512f7bbcadb50d2afbaf273
David Qui <dqalexruiz3@gmail.com>
2015-02-25 08:39:26 -0500

    cambio basico 4 (funciones)

--- codigo.js
+++ codigo.js
@@ -6,6 +6,7 @@ var valor = 0;
 var num1 = "";
 var num2 = 0;
 var resul = 0;
+
 for (i=0; i<n.value.length; i++)
 {
 	valor = s.substring(i,i+1);
@@ -35,7 +36,7 @@ for (i=0; i<n.value.length; i++)
 	   i++;
 	   num2 = s.substring(i,i+1);
        resul = (parseInt(resul) / parseInt(num2));
-	   }	   
+	   }	      
 	   
 	   else{
 		   num1 += valor;
@@ -49,7 +50,7 @@ var enc = false;
 function cambiar( v ) 
 {
 var valor =  document.getElementById("lcd");
-if(v != '+' && v != '-' && v != '*' && v != '/'){
+if(v != '+' && v != '-' && v != '*' && v != '/' && v != 'Sen (' ){
 valor.value += v;
 enc = false;
 }
@@ -72,4 +73,15 @@ var s = String(valor.value);
 valor.value = s.substring(0,s.length-1);
 }
 
-
+function funciones( v )
+{	
+	//el valor 0 representa la funcion sen
+    //el valor 1 representa la funcion cos
+	//el valor 2 representa la funcion tan
+	if(v=="0")
+      document.getElementById("lcd").value = Math.sin(parseInt(document.getElementById("lcd").value));
+	else if(v=="1")
+	   document.getElementById("lcd").value = Math.cos(parseInt(document.getElementById("lcd").value));
+    else if(v=="2")
+	   document.getElementById("lcd").value = Math.tan(parseInt(document.getElementById("lcd").value));
+}
